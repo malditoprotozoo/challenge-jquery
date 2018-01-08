5f62a1b7a85aa4230353bf243ee245875612582d
malditoprotozoo <virodriguez.fe@gmail.com>
2018-01-08 12:57:49 -0300

    Step 8: Changing classes on click in recipe.html

--- assets/js/script-recipe.js
+++ assets/js/script-recipe.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
   console.log("Loaded!");
   $(".js-menu").remove();
-})
+  changeClass();
+});
+
+var changeClass = (function() {
+  $(".js-show-make").click(function() {
+    $(".page").addClass("make");
+  });
+  $(".js-show-recipe").click(function() {
+    $(".page").removeClass("make");
+  })
+});
