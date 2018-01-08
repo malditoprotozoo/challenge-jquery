31bec44164cc81efeb12a185b86afb9a02ca40e5
malditoprotozoo <virodriguez.fe@gmail.com>
2018-01-08 10:10:58 -0300

    Step 2: Text added to News Section

--- assets/js/script-index.js
+++ assets/js/script-index.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
   $("#a-arrow-container").remove();
   $("#a-menu-container").remove();
+  printNews("Nuevas Recetas", newsParragraph);
 
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
 
 });
 
+/*
+* Declarando algunas variables útiles
+*/
+
+var newsParragraph = $("#p-news");
+
+/*
+* Función que añade un texto al parráfo en la sección de noticias
+*/
+var printNews = (function(text, container) {
+  $(container).append(text);
+  $(container).css("text-transform","uppercase");
+});
 
 /*
 * Función que se encarga de pintar TODAS las recetas que tengan 
