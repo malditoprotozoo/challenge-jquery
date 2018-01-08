6c8f51b2cd2481dd23a816f4b4fada9955ea90d4
malditoprotozoo <virodriguez.fe@gmail.com>
2018-01-08 13:00:06 -0300

    Step 9: Adding classes to the active buttons in recipe.html

--- assets/js/script-recipe.js
+++ assets/js/script-recipe.js
@@ -7,8 +7,12 @@ $(document).ready(function() {
 var changeClass = (function() {
   $(".js-show-make").click(function() {
     $(".page").addClass("make");
+    $(this).addClass("active");
+    $(".js-show-recipe").removeClass("active");
   });
   $(".js-show-recipe").click(function() {
     $(".page").removeClass("make");
+    $(this).addClass("active");
+    $(".js-show-make").removeClass("active");
   })
 });
